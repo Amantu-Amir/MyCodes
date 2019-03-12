493084c62d279a9483c1d48b9953bfb70ddf1a8f
Sarwar Jahan <Sarwar05@users.noreply.github.com>
2019-03-13 00:35:38 +0600

    Add files via upload

--- 501 - Black Box.cpp
+++ 501 - Black Box.cpp
@@ -1,28 +1,63 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-
+#include<bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
-
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-
-signed main() {
+#define mx 30005
+int tree[mx*4];
+void update(int pos, int b, int e, int i, int j)
+{
+    if(b>e || b>j || e<i) return;
+    if(b>=i && e<=j){
+        tree[pos] +=1;
+        return;
+    }
+    int m = (b+e)/2;
+    int l = pos*2;
+    int r = l+1;
+    update(l,b,m,i,j);
+    update(r, m+1, e, i, j);
+    tree[pos] = tree[l] + tree[r];
+}
+int findKth(int pos, int b, int e, int k)
+{
+    if(b==e) return b;
+    int m = (b+e)/2;
+    int l = pos*2;
+    int r = l+1;
+    if(tree[l]>=k) return findKth(l,b,m,k);
+    else return findKth(r, m+1, e, k - tree[l]);
+}
+int main() {
     int tc;
     scanf("%d", &tc);
     while(tc--){
         int n,m,a;
         scanf("%d%d", &n, &m);
         vector<int> arr(n);
-        for(int i=0; i<n; i++) scanf("%d", &arr[i]);
-        ordered_set st;
+        set<int> st;
+        for(int i=0; i<n; i++){
+            scanf("%d", &arr[i]);
+            st.insert(arr[i]);
+        }
+        int val = 1;
+        unordered_map<int, int>mp, anti;
+        for(auto x:st){
+            mp[x] = val;
+            anti[val] = x;
+            val++;
+        }
+        memset(tree, 0, sizeof tree);
         int done = 0, f = 0;
-        for(int i=1; i<=m; i++){
+        while(m--){
             scanf("%d", &a);
-            while(a>done)
-                st.insert(arr[done++]);
-            int ans = *st.find_by_order(f++);
+            while(done<a){
+                update(1,1,val,mp[ arr[done] ], mp[arr[done]]);
+                done++;
+            }
+            f++;
+            int ans = findKth(1,1,val,f);
+            ans = anti[ans];
             printf("%d\n", ans);
         }
+
         if(tc>0) printf("\n");
     }
 }
